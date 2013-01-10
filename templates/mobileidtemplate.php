a1ffe06817b04014bb7f91caa304a05dc969e72d
Freddy Kaiser <kaiser.freddy@gmail.com>
2013-01-11 00:00:34 +0100

    Error at the end refs #4

--- templates/mobileidtemplate.php
+++ templates/mobileidtemplate.php
@@ -3,21 +3,6 @@ $this->data['header'] = $this->t('{mobileid:Auth:header}');
 $this->data['autofocus'] = 'msisdn';
 $this->includeAtTemplateBase('includes/header.php');
 ?>
-
-<?php
-if ($this->data['errorcode'] !== NULL) {
-?>
-
-    <div style="border-left: 1px solid #e8e8e8; border-bottom: 1px solid #e8e8e8; background: #f5f5f5">
-		<img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/experience/gtk-dialog-error.48x48.png" class="float-l" style="margin: 15px " />
-		<h2><?php echo $this->t('{login:error_header}'); ?></h2>
-		<p><b><?php echo $this->t('{errors:title_' . $this->data['errorcode'] . '}'); ?></b></p>
-		<p><?php echo $this->t('{errors:descr_' . $this->data['errorcode'] . '}'); ?></p>
-	</div>
-
-<?php
-}
-?>
 	<img height="100" style="float: right" src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/logo.gif')); ?>" />
 	<h2 style=""><?php echo $this->t('{mobileid:Auth:header}'); ?></h2>
 	<form action="?" method="post" name="f">
@@ -29,6 +14,21 @@ foreach ($this->data['stateparams'] as $name => $value) {
 }
 ?>
 	</form>
+
+<?php
+    if ($this->data['errorcode'] !== NULL) {
+?>
+
+<div style="border-left: 1px solid #e8e8e8; border-bottom: 1px solid #e8e8e8; background: #f5f5f5">
+<img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/experience/gtk-dialog-error.48x48.png" class="float-l" style="margin: 15px " />
+<h2><?php echo $this->t('{login:error_header}'); ?></h2>
+<p><b><?php echo $this->t('{errors:title_' . $this->data['errorcode'] . '}'); ?></b></p>
+<p><?php echo $this->t('{errors:descr_' . $this->data['errorcode'] . '}'); ?></p>
+</div>
+
+<?php
+}
+?>
 
 <?php
 // $this->includeAtTemplateBase('includes/footer.php');
