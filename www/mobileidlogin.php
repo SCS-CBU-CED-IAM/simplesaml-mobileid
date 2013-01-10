6ca48a19fe6de5e8df5abc1f31ba354f3a30a77f
Freddy Kaiser <kaiser.freddy@gmail.com>
2013-01-10 21:27:07 +0100

    Check set of lang param refs #4

--- www/mobileidlogin.php
+++ www/mobileidlogin.php
@@ -8,22 +8,23 @@
  * @package midsimplesamlphp
  * @version $Id$
  */
+private $language;
+private $msisdn = '';
 
+/* Get AuthState and AuthStateID */
 if (!array_key_exists('AuthState', $_REQUEST))
     throw new SimpleSAML_Error_BadRequest('Missing AuthState parameter.');
-
 $authStateId = $_REQUEST['AuthState'];
 
 /* Language detection */
-$language = $_GET['lang'];
+if (isset($_GET['lang']))
+    $language = $_GET['lang'];
 if (!strlen($language))
     $language = strtolower(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
 
 /* MSISDN default value */
 if (array_key_exists('msisdn', $_REQUEST))
     $msisdn = $_REQUEST['msisdn'];
-else
-    $msisdn = '';
 
 /* Try to login */
 if (!empty($msisdn))
