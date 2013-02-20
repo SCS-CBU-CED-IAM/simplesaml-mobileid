22d877c981c78619815ef885f4296480f8065908
Jerome <jerome.michellod@gmail.com>
2013-02-20 18:24:01 +0100

    - Issue #19

--- templates/mobileidtemplate.php
+++ templates/mobileidtemplate.php
@@ -9,50 +9,49 @@
  * @author      Swisscom (Schweiz AG)
  */
 
-$this->data['head']  = '<link rel="stylesheet" media="screen" type="text/css" href="' . SimpleSAML_Module::getModuleUrl('mobileid/resources/css/bootstrap.min.css')  . '" />';
-$this->data['head'] .= '<link rel="stylesheet" media="screen" type="text/css" href="' . SimpleSAML_Module::getModuleUrl('mobileid/resources/css/bootstrap-responsive.min.css')  . '" />';
-$this->data['head'] .= '<link rel="stylesheet" media="screen" type="text/css" href="' . SimpleSAML_Module::getModuleUrl('mobileid/resources/css/custom.css')  . '" />';
 $this->data['head'] .= '<script type="text/javascript" src="' . SimpleSAML_Module::getModuleUrl('mobileid/resources/js/jquery/jquery-1.8.3.min.js') . '"></script>';
-$this->data['head'] .= '<script type="text/javascript" src="' . SimpleSAML_Module::getModuleUrl('mobileid/resources/js/bootstrap.min.js') . '"></script>';
 $this->data['head'] .= '<script type="text/javascript" src="' . SimpleSAML_Module::getModuleUrl('mobileid/resources/js/mobileid.js') . '"></script>';
 $this->data['header'] = $this->t('{mobileid:Auth:header}');
 $this->data['autofocus'] = 'msisdn';
-$this->data['hideLanguageBar'] = true;
 $this->includeAtTemplateBase('includes/header.php');
 ?>
-<img class="logo" src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/logo.gif')); ?>" />
+<div style="border-left: 1px solid #e8e8e8; border-bottom: 1px solid #e8e8e8; background: #f5f5f5; display:none;" id="msg_error">
+	<img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/experience/gtk-dialog-error.48x48.png" class="float-l" style="margin: 15px " />
+	<h2><?php echo $this->t('{mobileid:errors:error_header}'); ?></h2>
+	<p><?php echo $this->t('{mobileid:errors:descr_' . $this->data['errorcode'] . '}'); ?></p>
+</div>
 <h2 style=""><?php echo $this->t('{mobileid:Auth:header}'); ?></h2>
 <form action="?" method="post" name="f" id="mobileid_form">
-    <p><?php echo $this->t('{mobileid:Auth:intro}'); ?></p>
-	<fieldset>
-		<div class="control-group">
-			<div class="controls">
-				<input id="msisdn" name="msisdn" class="msisdn" type="tel" required />
-			</div>
-		</div>
-		<div class="form-actions">
-			<input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_send}'); ?>" class="btn" id="submit_btn_send" />
-			<input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_cancel}'); ?>" class="btn" id="submit_btn_cancel" />
-		</div>
-		<?php
-		foreach ($this->data['stateparams'] as $name => $value) {
-			echo('<input type="hidden" name="' . htmlspecialchars($name) . '" value="' . htmlspecialchars($value) . '" />');
-		}
-		?>
-	</fieldset>
+	<table>
+		<tbody>
+			<tr width="100%">
+				<td rowspan="2"><img style="height:50px; padding:2px;" src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/logo.gif')); ?>" /></td>
+				<td style="padding: .3em;"><?php echo $this->t('{mobileid:Auth:intro}'); ?></td>
+				<td><input id="msisdn" name="msisdn" tabindex="1" class="msisdn" type="tel" /></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>
+					<input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_cancel}'); ?>" class="float-l" id="submit_btn_cancel" />
+					<input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_send}'); ?>" class="float-r" id="submit_btn_send" />
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<?php
+	foreach ($this->data['stateparams'] as $name => $value) {
+		echo('<input type="hidden" name="' . htmlspecialchars($name) . '" value="' . htmlspecialchars($value) . '" />');
+	}
+	?>
 </form>
-<div id="msg_wait">
+<div id="msg_wait" style="display:none;">
 	<img src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/ajax-loader.gif')); ?>" alt="<?php echo $this->t('{mobileid:Auth:msg_wait}'); ?>" title="<?php echo $this->t('{mobileid:Auth:msg_wait}'); ?>" />
 	<p><?php echo $this->t('{mobileid:Auth:msg_wait}'); ?></p>
 </div>
 <?php if ($this->data['errorcode'] !== NULL) { ?>
 <script>
 	jQuery('#msg_wait').hide();
+	jQuery('#msg_error').show();
 </script>
-<div id="msg_error" class="alert alert-block alert-error">
-	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<strong><?php echo $this->t('{mobileid:errors:error_header}'); ?></strong>
-	<p><?php echo $this->t('{mobileid:errors:descr_' . $this->data['errorcode'] . '}'); ?></p>
-</div>
 <?php } ?>
 <?php $this->includeAtTemplateBase('includes/footer.php'); ?>
