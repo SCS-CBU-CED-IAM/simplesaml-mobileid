add1441131f007b2601f367f64929b6c39050ef6
Jerome <jerome.michellod@gmail.com>
2013-02-26 15:15:50 +0100

    - Issue #17

--- www/mobileidlogin.php
+++ www/mobileidlogin.php
@@ -16,29 +16,26 @@ if (!array_key_exists('AuthState', $_REQUEST))
 
 $authStateId = $_REQUEST['AuthState'];
 
-/* Retrieve the authentication state. */
-$state = SimpleSAML_Auth_State::loadState($authStateId, sspmod_mobileid_Auth_Source_Auth::STAGEID);
-
 /* MSISDN default value */
 if (array_key_exists('msisdn', $_REQUEST)) {
     $msisdn = $_REQUEST['msisdn'];
+}
 
-	/* Remember the mobile number */
-	if ($state['remember_msisdn']) {
-		$sessionHandler = SimpleSAML_SessionHandler::getSessionHandler();
-		$params = $sessionHandler->getCookieParams();
-		$params['expire']  = time();
-		$params['expire'] += 31536000;
-		$_COOKIE['msisdn'] = $msisdn;
-		setcookie('msisdn', $msisdn, $params['expire'], $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+/* Retrieve the authentication state. */
+$state = SimpleSAML_Auth_State::loadState($authStateId, sspmod_mobileid_Auth_Source_Auth::STAGEID);
+
+/* Remember the mobile number */
+if (isset($state['remember_msisdn']) && isset($msisdn)) {
+	if ($state['remember_msisdn']) {	// Config is set and true
+		setCookies($msisdn);
+	} else {							// Config is set but false
+		removeCookies();
 	}
 }
 
-if (!$state['remember_msisdn']) {
-	if (isset($_COOKIE['msisdn'])) {
-		unset($_COOKIE['msisdn']);
-		setcookie('msisdn', '', time()-3600);
-	}
+// Config is not set
+if (!isset($state['remember_msisdn'])) {
+	removeCookies();
 }
     
 /* Login and results */
@@ -58,5 +55,20 @@ $t->data['stateparams'] = array('AuthState' => $authStateId);
 $t->data['errorcode'] = $errorCode;
 $t->show();
 exit();
-    
+  
+function setCookies($msisdn) {
+	$sessionHandler = SimpleSAML_SessionHandler::getSessionHandler();
+	$params = $sessionHandler->getCookieParams();
+	$params['expire']  = time();
+	$params['expire'] += 31536000;
+	$_COOKIE['msisdn'] = $msisdn;
+	setcookie('msisdn', $msisdn, $params['expire'], $params['path'], $params['domain'], $params['secure'], $params['httponly']);	
+}
+
+function removeCookies() {
+	if (isset($_COOKIE['msisdn'])) {
+		unset($_COOKIE['msisdn']);
+		setcookie('msisdn', '', time()-3600);
+	}	
+}
 ?>
