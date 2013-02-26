d4aa9ea14aea726ac1d894988e321e825b9b302d
Aji Yahya <aji.yahya@abacus.ch>
2013-02-26 14:18:33 +0100

    Content-Layout changes

--- templates/mobileidtemplate.php
+++ templates/mobileidtemplate.php
@@ -15,11 +15,17 @@ $this->data['header'] = $this->t('{mobileid:Auth:header}');
 $this->data['autofocus'] = 'msisdn';
 $this->includeAtTemplateBase('includes/header.php');
 ?>
-<div style="border-left: 1px solid #e8e8e8; border-bottom: 1px solid #e8e8e8; background: #f5f5f5; display:none;" id="msg_error">
-	<img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/experience/gtk-dialog-error.48x48.png" class="float-l" style="margin: 15px " />
-	<h2><?php echo $this->t('{mobileid:errors:error_header}'); ?></h2>
+
+<img style="height:100px; display:inline;padding:0px;margin:0 20px 0 0; float:right;" src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/logo.gif')); ?>" />
+
+<div class="mobileid-main">
+<h2 style=""><?php echo $this->t('{mobileid:Auth:header}'); ?></h2>
+<div id="msg_error" class="alert alert-block alert-error">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+	<strong><?php echo $this->t('{mobileid:errors:error_header}'); ?></strong>
 	<p><?php echo $this->t('{mobileid:errors:descr_' . $this->data['errorcode'] . '}'); ?></p>
 </div>
+<<<<<<< HEAD
 <h2 style=""><?php echo $this->t('{mobileid:Auth:header}'); ?>
 	<img style="height:80px; padding:0px;margin:0 30px 0 0; float:right;" src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/logo.gif')); ?>" />
 </h2>
@@ -39,6 +45,24 @@ $this->includeAtTemplateBase('includes/header.php');
 			</tr>
 		</tbody>
 	</table>
+=======
+<form action="?" method="post" name="f" id="mobileid_form">
+	
+              <div class="control-group">
+			<div class="mobileid-controls">
+				<h4><?php echo $this->t('{mobileid:Auth:intro}'); ?></h4>
+				<input id="msisdn" name="msisdn" tabindex="1" class="mobileid-msisdn" type="tel" value="<?php if (isset($msisdn_cookie)) echo $msisdn_cookie; ?>" />
+                                <input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_send}'); ?>" class="float-r mobileid-btn-send" id="submit_btn_send" />
+			</div>
+               </div>
+                                
+                 <div class="form-actions">       
+                        
+                       <input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_cancel}'); ?>" class="float-l mobileid-btn-cancel" id="submit_btn_cancel" />
+			
+		</div>
+                                        
+>>>>>>> Content-Layout changes
 	<?php
 	foreach ($this->data['stateparams'] as $name => $value) {
 		echo('<input type="hidden" name="' . htmlspecialchars($name) . '" value="' . htmlspecialchars($value) . '" />');
@@ -48,6 +72,7 @@ $this->includeAtTemplateBase('includes/header.php');
 <div id="msg_wait" style="display:none;">
 	<img class="mobileid-ajax-loader" src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/ajax-loader.gif')); ?>" alt="<?php echo $this->t('{mobileid:Auth:msg_wait}'); ?>" title="<?php echo $this->t('{mobileid:Auth:msg_wait}'); ?>" />
 	<p><?php echo $this->t('{mobileid:Auth:msg_wait}'); ?></p>
+</div>
 </div>
 <?php if ($this->data['errorcode'] !== NULL) { ?>
 <script>
