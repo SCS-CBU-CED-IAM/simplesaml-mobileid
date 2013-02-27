a1fe3174f131167807006ede480a8dfc35abf0fb
Philipp Hug <philipp@hug.cx>
2013-02-27 10:55:01 +0100

    Move template changes into abacus theme. refs #19
    Revert "Content Table based to Div and css"
    
    This reverts commit a7ec2f5ab7e9940b750b84eaed814f39c42c5ed1.

--- templates/mobileidtemplate.php
+++ templates/mobileidtemplate.php
@@ -19,37 +19,30 @@ if ($this->data['errorcode'] !== NULL && array_key_exists('msisdn', $_REQUEST)) 
 	$_COOKIE["msisdn"] = $_REQUEST['msisdn'];
 }
 ?>
-
-<img style="height:100px; display:inline;padding:0px;margin:0 20px 0 0; float:right;" src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/logo.gif')); ?>" />
-
-<div class="mobileid-main">
-<h2 style=""><?php echo $this->t('{mobileid:Auth:header}'); ?></h2>
-<div id="msg_error" class="alert alert-block alert-error">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-	<strong><?php echo $this->t('{mobileid:errors:error_header}'); ?></strong>
+<div style="border-left: 1px solid #e8e8e8; border-bottom: 1px solid #e8e8e8; background: #f5f5f5; display:none;" id="msg_error">
+	<img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/experience/gtk-dialog-error.48x48.png" class="float-l" style="margin: 15px " />
+	<h2><?php echo $this->t('{mobileid:errors:error_header}'); ?></h2>
 	<p><?php echo $this->t('{mobileid:errors:descr_' . $this->data['errorcode'] . '}'); ?></p>
 </div>
-
-
-
+<h2 style=""><?php echo $this->t('{mobileid:Auth:header}'); ?>
+	<img style="height:80px; padding:0px;margin:0 30px 0 0; float:right;" src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/logo.gif')); ?>" />
+</h2>
 <form action="?" method="post" name="f" id="mobileid_form">
-	
-              <div class="control-group">
-                  <h4><?php echo $this->t('{mobileid:Auth:intro}'); ?></h4>
-			<div class="mobileid-controls">
-				
-				<input id="msisdn" size="30" name="msisdn" tabindex="1" class="msisdn mobileid-input" type="tel" value="<?php if (isset($_COOKIE["msisdn"])) echo $_COOKIE["msisdn"]; ?>" />        
-			</div>
-                        <div class="form-actions">       
-                             <input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_send}'); ?>" class="float-r mobileid-btn-send" id="submit_btn_send" />
-                             <input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_cancel}'); ?>" class="float-l mobileid-btn-cancel" id="submit_btn_cancel" />
-
-                      </div>
-               </div>
-                                
-                 
-                                        
-
+	<table>
+		<tbody>
+			<tr width="100%">
+				<td style="padding: .3em;"><?php echo $this->t('{mobileid:Auth:intro}'); ?></td>
+				<td><input id="msisdn" size="30" name="msisdn" tabindex="1" class="msisdn mobileid-input" type="tel" value="<?php if (isset($_COOKIE["msisdn"])) echo $_COOKIE["msisdn"]; ?>" /></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>
+					<input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_cancel}'); ?>" class="float-l mobileid-btn-cancel" id="submit_btn_cancel" />
+					<input type="button" value="<?php echo $this->t('{mobileid:Auth:form_btn_send}'); ?>" class="float-r mobileid-btn-send" id="submit_btn_send" />
+				</td>
+			</tr>
+		</tbody>
+	</table>
 	<?php
 	foreach ($this->data['stateparams'] as $name => $value) {
 		echo('<input type="hidden" name="' . htmlspecialchars($name) . '" value="' . htmlspecialchars($value) . '" />');
@@ -59,7 +52,6 @@ if ($this->data['errorcode'] !== NULL && array_key_exists('msisdn', $_REQUEST)) 
 <div id="msg_wait" style="display:none;">
 	<img class="mobileid-ajax-loader" src="<?php echo(SimpleSAML_Module::getModuleURL('mobileid/resources/ajax-loader.gif')); ?>" alt="<?php echo $this->t('{mobileid:Auth:msg_wait}'); ?>" title="<?php echo $this->t('{mobileid:Auth:msg_wait}'); ?>" />
 	<p><?php echo $this->t('{mobileid:Auth:msg_wait}'); ?></p>
-</div>
 </div>
 <?php if ($this->data['errorcode'] !== NULL) { ?>
 <script>
