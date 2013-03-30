ec2f67f0fad958478cd8647feb042c43d7fb6250
SCS-CBU-CED-IAM <freddy.kaiser@swisscom.com>
2013-03-30 11:36:12 +0100

    Enhanced cancel refs #1

--- www/cancel.php
+++ www/cancel.php
@@ -1,4 +1,26 @@
 <?php
+/**
+* This page handles the user cancel
+*
+* @version     1.0.0
+* @package     simpleSAMLphp-mobileid
+* @copyright   Copyright (C) 2012. All rights reserved.
+* @license     GNU General Public License version 2 or later; see LICENSE.md
+* @author      Swisscom (Schweiz AG)
+*/
+    
+/* Get AuthState and AuthStateID */
+if (!array_key_exists('AuthState', $_REQUEST))
+    throw new SimpleSAML_Error_BadRequest('Missing AuthState parameter.');
+    
+$authStateId = $_REQUEST['AuthState'];
+    
+/* Retrieve the authentication state */
+$state = SimpleSAML_Auth_State::loadState($authStateId, sspmod_mobileid_Auth_Source_Auth::STAGEID);
+
+/* User cancel */
 $e = new SimpleSAML_Error_UserAborted();
-SimpleSAML_Auth_State::throwException(array(), $e);
+SimpleSAML_Auth_State::throwException($state, $e);
+
+// SimpleSAML_Auth_Source::completeAuth($state);
 ?>
